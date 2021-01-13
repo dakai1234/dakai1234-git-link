752fa37a0c85dec7078e6f92c17a52d277aba839
dakai <1074889192@qq.com>
2021-01-13 15:59:07 +0800

    小测试

---
+++ test2/src/com/smaerlab/test2/test2.java
@@ -0,0 +1,11 @@
+package com.smaerlab.test2;
+
+public class test2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int i = 0;
+		System.out.println("iÖµÎª£º"+i);
+	}
+
+}
