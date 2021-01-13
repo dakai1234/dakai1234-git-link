1c1801679a58920c49cebe0c2f28e130726ba023
dakai <1074889192@qq.com>
2021-01-13 19:57:57 +0800

    12345

---
+++ sTest/src/com/smartlab/sTest/sTest.java
@@ -0,0 +1,10 @@
+package com.smartlab.sTest;
+
+public class sTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("hello world!");
+	}
+
+}
